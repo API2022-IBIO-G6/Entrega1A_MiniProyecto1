61f409037d6adaa03deb60cb8aa01c8911969740
Lina-go <77931393+Lina-go@users.noreply.github.com>
2022-02-09 17:01:24 -0500

    commit

--- app/prueba.py
+++ app/prueba.py
@@ -1,13 +1,9 @@
 import os
 import skimage.io
+import random
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import glob
-
-
-
-# Funcion para subir imágenes 
-
 
 train = [f for f in glob.glob(os.path.join('data/train','*.jpg'))]
 test = [f for f in glob.glob(os.path.join('data/test','*.jpg'))]
@@ -17,6 +13,7 @@ img = mpimg.imread(os.path.join(test[0]))
 imgplot = plt.imshow(img)
 
 for i in range(0,8):
+    #numeroAleatorio = random.randint(0,len(test)-1)
     plt.subplot(2,4,i+1)
     img = mpimg.imread(os.path.join(train[i]))
     imgplot = plt.imshow(img)
