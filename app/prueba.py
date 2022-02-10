f64ed53735bfb33d2d8bfef44e4ec07d9fd52069
Lina-go <77931393+Lina-go@users.noreply.github.com>
2022-02-09 20:47:13 -0500

    commit

--- app/prueba.py
+++ app/prueba.py
@@ -8,9 +8,9 @@ import json
 import utils as ut
 
 # Importan las imagenes de la carpeta
-train = [f for f in glob.glob(os.path.join('data/train','*.jpg'))]
-test = [f for f in glob.glob(os.path.join('data/test','*.jpg'))]
-valid = [f for f in glob.glob(os.path.join('data/valid','*.jpg'))]
+train = [f for f in glob.glob(os.path.join('data\\train','*.jpg'))]
+test = [f for f in glob.glob(os.path.join('data\\test','*.jpg'))]
+valid = [f for f in glob.glob(os.path.join('data\\valid','*.jpg'))]
 
 # Generar el primer subplot
 #img = mpimg.imread(os.path.join(test[0]))
@@ -21,7 +21,7 @@ for i in range(0,8):
     #numeroAleatorio = random.randint(0,len(test)-1)
     plt.subplot(2,4,i+1)
     img = mpimg.imread(os.path.join(train[i]))
-    print(train[i])
+    print(train[0])
     imgplot = plt.imshow(img)
     plt.title("Imagen"+str(i+1))
     plt.axis('off')
@@ -29,7 +29,7 @@ plt.suptitle("Imagenes de entrenamiento")
 plt.show()
 
 # Leer el archivo JSON
-with open(os.path.join("data/train","_annotations.coco.json")) as json_file:
+with open(os.path.join("data\\train","_annotations.coco.json")) as json_file:
     data = json.load(json_file)
     
-#ut.visualize_annotations(fold='train', img_name=train[0], annotations_json_name='_annotations.coco.json', interest_class=-1)
+print(ut.visualize_annotations(fold='train', img_name=train[0], annotations_json_name='_annotations.coco.json', interest_class=-1))
